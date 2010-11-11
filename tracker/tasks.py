80f7c0844562f2ab7173ab4bf634cd5b91650769
Batiste Bieler <batisteb@opera.com>
2010-11-11 15:56:22 +0100

    The task takes the collect time from the model.

--- tracker/tasks.py
+++ tracker/tasks.py
@@ -1,8 +1,8 @@
 from datetime import timedelta
 from celery.decorators import task, periodic_task
-from tracker.models import make_daily_report
+from tracker.models import make_daily_report, COLLECT_TIME
 
-@periodic_task(run_every=timedelta(seconds=120))
+@periodic_task(run_every=timedelta(seconds=COLLECT_TIME))
 def collect_statistic():
     make_daily_report()
 
